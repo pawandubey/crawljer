81bb6a2da486e76c0def8ddc2d7c9bdc1ce480e8
Pawan Dubey <pawanthegunner@gmail.com>
2017-03-25 09:20:27 +0530

    Add the :main ns and dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"
             :year 2016
             :key "apache-2.0"}
+  :main crawljer.core
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.395"]
                  [clj-http "2.3.0"]
-                 [enlive "1.1.6"]])
+                 [enlive "1.1.6"]
+                 [me.raynes/fs "1.4.6"]])
